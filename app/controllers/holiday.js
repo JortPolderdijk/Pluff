70587238d2d9603bc0775a363befc97480d7e5af
JortPolderdijk <j.polderdijk@student.fontys.nl>
2017-08-05 19:04:48 +0200

    Prepared branch for new version

--- app/controllers/holiday.js
+++
@@ -1,6 +0,0 @@
-export default function ($scope, holidayService) {
-  // Load the holiday JSON and insert it in the scope
-  holidayService.getHolidays().then((payload) => {
-    $scope.holidays = payload;
-  });
-}
--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-  plugins: [
-    autoprefixer({ browsers: ['last 1 versions'] }),
-  ],
-};
