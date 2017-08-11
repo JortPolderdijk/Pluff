d968fa681b7c5f9b3b8f39aa1f962685b5513e6e
JortPolderdijk <j.polderdijk@student.fontys.nl>
2017-08-11 22:31:31 +0200

    Implemented routing and added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 
+import { TimetableComponent } from './timetable/timetable.component';
+import { RoomsComponent } from './rooms/rooms.component';
+
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'}
+  { path: '', redirectTo: '/timetable', pathMatch: 'full' },
+  { path: 'timetable', component: TimetableComponent },
+  { path: 'rooms', component: RoomsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,25 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { OAuthModule } from 'angular-oauth2-oidc';
 import { ApiClientService } from './fhict-api-service';
 import { AuthGuard } from './auth/auth.guard';
+import { TimetableComponent } from './timetable/timetable.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    TimetableComponent,
+    RoomsComponent,
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    AppRoutingModule,
     OAuthModule.forRoot()
   ],
   providers: [
