66e5ab7e129f887960f2e699f581deaa0d277b85
Cas van Dongen <cas.vandongen@student.fontys.nl>
2018-02-08 17:28:03 +0100

    Removed redundant const

--- src/pipes/ScheduleItemParts.ts
+++ src/pipes/ScheduleItemParts.ts
@@ -3,8 +3,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'scheduleparts'})
 export class ScheduleItemParts implements PipeTransform {
   transform(value: any, end: any): number {
-    const difference = Math.ceil(Math.abs(new Date(value).getTime() - new Date(end).getTime()) / (1000 * 60));
-    return difference;
+    return Math.ceil(Math.abs(new Date(value).getTime() - new Date(end).getTime()) / (1000 * 60));
   }
 
 }
