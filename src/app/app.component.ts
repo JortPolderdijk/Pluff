025c57e5687feb84bbc0f26d10970df56e8fb9d2
Cas van Dongen <cas.vandongen@student.fontys.nl>
2018-02-08 13:50:29 +0100

    Main navigation styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {LoginModalComponent} from './modal/login/login.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
